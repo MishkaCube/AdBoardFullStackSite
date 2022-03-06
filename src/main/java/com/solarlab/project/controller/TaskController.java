6559e5452c46f12be806391d57d30c7c5d483890
Michail Tsibrankov <mishka.barman@gmail.com>
2022-03-06 18:28:48 +0300

    Retake a project Started to learning REST

--- src/main/java/com/solarlab/project/controller/TaskController.java
+++ src/main/java/com/solarlab/project/controller/TaskController.java
@@ -1,29 +1,25 @@
 package com.solarlab.project.controller;
 
-import com.solarlab.project.dto.PersonDto;
-import lombok.RequiredArgsConstructor;
+import com.solarlab.project.user.user;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/people")
+import java.util.List;
+
+
+@RestController
 public class TaskController {
-    private final PersonDto personDto;
 
-    @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("people", personDto.index());
-        return "people/index";
-    }
-
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id,
-                       Model model) {
-        model.addAttribute("person", personDto.show(id));
-        return "people/show ";
+    @GetMapping("/people")
+    public List<user> start() {
+        return List.of(
+                new user(
+                        1L,
+                        "Michael",
+                        "michael@gmail.com",
+                        13,
+                        15
+                ));
     }
 }
