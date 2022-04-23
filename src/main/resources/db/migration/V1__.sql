b05833985c0dc3c7f8e46a94c4794c7d370f1fe0
Michail Tsibrankov <mishka.barman@gmail.com>
2022-04-23 17:17:07 +0300

    Clean and configure FW

--- src/main/resources/db/migration/V1__.sql
+++ src/main/resources/db/migration/V1__.sql
@@ -12,35 +12,4 @@ create TABLE advertisments (
    CONSTRAINT pk_advertisments PRIMARY KEY (id)
 );
 
-create TABLE authorities (
-  username VARCHAR(255) NOT NULL,
-   authority VARCHAR(255),
-   CONSTRAINT pk_authorities PRIMARY KEY (username)
-);
-
-create TABLE oauth_client_details (
-  client_id VARCHAR(255) NOT NULL,
-   resource_ids VARCHAR(255),
-   client_secret VARCHAR(255),
-   authorized_grant_types VARCHAR(255),
-   web_server_redirect_uri VARCHAR(255),
-   access_token_validity INTEGER,
-   refresh_token_validity INTEGER,
-   additional_information VARCHAR(255),
-   scope VARCHAR(255),
-   authorities VARCHAR(255),
-   autoapprove BOOLEAN,
-   CONSTRAINT pk_oauth_client_details PRIMARY KEY (client_id)
-);
-
-create TABLE users (
-  id BIGINT NOT NULL,
-   name VARCHAR(255),
-   second_name VARCHAR(255),
-   user_email VARCHAR(255),
-   date_of_birth date,
-   user_password VARCHAR(255),
-   age INTEGER,
-   CONSTRAINT pk_users PRIMARY KEY (id)
-);
 
